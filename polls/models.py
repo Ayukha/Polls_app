bb5ee4f4568d4a65430e6943cdda9d5bc7858bf5
Ayukha <ayussh.khare@gmail.com>
2017-08-22 18:59:19 +0530

    Polling feature added

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ import datetime
 from django.utils import timezone
 
 from django.db import models
-# from django.utils.encoding import python_2_unicode_compatible
+
 
 
 # Create your models here.
@@ -12,7 +12,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    # @python_2_unicode_compatible  # only if you need to support Python 2
+    
     def __str__(self):
         return unicode(self.question_text)
 
@@ -24,6 +24,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    # @python_2_unicode_compatible  # only if you need to support Python 2
+    
     def __str__(self):
         return unicode(self.choice_text)
