c01aa4af2b4c0b988cf1c26b091ee22fe4da6e5a
Ayukha <ayussh.khare@gmail.com>
2017-08-24 19:17:48 +0530

    Tests Added

--- polls/views.py
+++ polls/views.py
@@ -11,6 +11,8 @@ from django.views import generic
 
 from .models import Choice, Question
 
+from django.utils import timezone
+
 
 
 
@@ -22,7 +24,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
