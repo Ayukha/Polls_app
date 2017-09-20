394c2bd6d0e567daab83621c85209b3413601a1c
Ayukha <ayussh.khare@gmail.com>
2017-09-20 18:20:02 +0530

    REST API added

--- polls/api/urls.py
+++ polls/api/urls.py
@@ -4,7 +4,7 @@ from polls.api.views import QuestionListAPIView , ChoiceListAPIView
 
 urlpatterns = [
     url(r'^$', QuestionListAPIView.as_view(), name='question'),
-    url(r'^$', ChoiceListAPIView.as_view(), name='choice'),
+    url(r'^show$', ChoiceListAPIView.as_view(), name='choice'),
     #url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     #url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
